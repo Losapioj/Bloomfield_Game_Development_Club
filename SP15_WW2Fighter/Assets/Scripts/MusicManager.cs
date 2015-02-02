e9b3f58b5867fca2259c317c16c40810fd25e1a5
Losapioj <Losapioj@gmail.com>
2015-02-02 16:19:07 -0500

    project 1 setup

---
+++ SP15_WW2Fighter/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MusicManager : MonoBehaviour {
+
+	public List<AudioClip> audioClips = new List<AudioClip>();
+	private Dictionary<string, AudioClip> _clipDictionary = new Dictionary<string, AudioClip>();
+	
+	private bool _isFadingIn = false;
+	private bool _isFadingOut = false;
+//	private;
+
+	//SINGLETON//
+	private static MusicManager _instance;
+	
+	public static MusicManager instance
+	{
+		get
+		{
+			if(_instance == null)
+			{
+				_instance = GameObject.FindObjectsOfType<MusicManager>();
+				
+				//if persistant
+				DontDestroyOnLoad(_instance.gameObject);
+			}
+			return _instance;
+		}
+	}
+	
+	void Awake()
+	{
+		if(_instance == null)
+		{
+			_instance = this;
+			DontDestroyOnLoad(this);
+		}
+		else
+		{
+			if(this != _instance)
+				Destroy(this.gameObject);
+		}
+		
+		foreach (AudioClip a in audioClips)
+		{
+			_clipDictionary.Add(a.name, a);
+		}
+	}
+
+	public void Play(string name)
+	{
+		//play audio requested
+		_clipDictionary[name].
+	}
+}
---
+++ SP15_WW2Fighter/Assets/Scripts/Object Factory.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectFactory : MonoBehaviour {
+	public List<GameObject> items = new List<GameObject>();
+	private Dictionary<string, GameObject> factory = new Dictionary<string, GameObject>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		foreach (GameObject g in items)
+		{
+			factory.Add(g.name, g.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	
+	public GameObject BuildObjecct(string n, Vector3 pos, Quaternion rot)
+	{
+		return Instantiate(factory[n], pos, rot) as GameObject;
+	}
+}
